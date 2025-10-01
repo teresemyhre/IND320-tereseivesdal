98c1df7a4975ea2b80fe3c27668e77d49bc1fe13
teresemyhre <terese.myhre@nmbu.no>
2025-10-01 20:11:06 +0200

    added app-file

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Hello IND320 👋")
+
+st.write("This is a minimum working example of a Streamlit app.")
+
+# load your csv (make sure it's in the repo, e.g. in `data/`)
+# df = pd.read_csv("data/open-meteo-subset.csv")
+# st.write("First 5 rows of the CSV:")
+
+# st.dataframe(df.head())
