6b341cadef59d1a6af03b4a64da8df64e948aa20
teresemyhre <terese.myhre@nmbu.no>
2025-10-02 22:55:32 +0200

    new

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import altair as alt
 
-custom_colors = ["#fd9e53", "#ffcea8", "#6CA0DC", "#9ecaec", "#3b97da"]
+custom_colors = ["#fd9e53", "#ffcea8", "#416287", "#9ecaec", "#3b97da"]
 
 @alt.theme.register('custom_theme', enable=True)
 def custom_theme():
