0f8b307844dcead16e550e548e8c1abd0b05b3c3
teresemyhre <terese.myhre@nmbu.no>
2025-10-02 22:49:41 +0200

    update

--- pages/04_blank.py
+++ pages/04_blank.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("Image Page")
-
 # Display an image from the local folder
-st.image("images/Untitled (Draft)-1 2.jpg", use_column_width=True)
+st.image("images/drawing.jpg", use_column_width=True)
